3c028e81b54239f167a2de21871c6721cc50b463
harry <me@harry.plus>
2020-04-02 15:14:43 +0530

    Show as Mbps. Use U/D instead of arrows.

--- scripts/network-bandwidth.sh
+++ scripts/network-bandwidth.sh
@@ -49,7 +49,9 @@ get_bandwidth() {
 
 format_speed() {
   local padding=$(get_tmux_option "@tmux-network-bandwidth-padding" 5)
-  numfmt --to=iec-i --suffix "B/s" --format "%f" --padding $padding $1
+  #numfmt --to=iec-i --suffix "B/s" --format "%f" --padding $padding $1
+  #echo $(echo "scale=2; $1/125000" | bc -l) "Mbps"
+  printf '%.2f Mbps\n' "$(echo "scale=2; $1/125000" | bc)"
 }
 
 main() {
@@ -67,7 +69,7 @@ main() {
     local second_measure=( $(get_bandwidth $os) )
     local download_speed=$(((${second_measure[0]} - ${first_measure[0]}) / $sleep_time))
     local upload_speed=$(((${second_measure[1]} - ${first_measure[1]}) / $sleep_time))
-    $(set_tmux_option "@network-bandwidth-previous-value" "↓$(format_speed $download_speed) • ↑$(format_speed $upload_speed)")
+    $(set_tmux_option "@network-bandwidth-previous-value" "D: $(format_speed $download_speed) • U: $(format_speed $upload_speed)")
   fi
 
   echo -n "$(get_tmux_option "@network-bandwidth-previous-value")"
